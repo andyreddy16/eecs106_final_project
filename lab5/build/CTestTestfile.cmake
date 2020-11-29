5e332b7cc2be7d8b5707bd376d35f2de45d8a932
Akash Velu <akashvelu@berkeley.edu>
2020-11-28 21:41:51 -0800

    import lab5

---
+++ lab5/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/akashvelu/ros_workspaces/lab5/src
+# Build directory: /home/akashvelu/ros_workspaces/lab5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(ik)
+subdirs(planning)
