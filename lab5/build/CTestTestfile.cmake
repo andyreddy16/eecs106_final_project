6ecae408f910571c5be97d598d8396eb6f586b69
Andy Reddy <andyreddy@Andys-MacBook-Pro.local>
2020-11-28 22:01:15 -0800

    Removed lab5 bc Akash couldnt

--- lab5/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/akashvelu/ros_workspaces/lab5/src
-# Build directory: /home/akashvelu/ros_workspaces/lab5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(ik)
-subdirs(planning)
--- lab5/build/planning/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/akashvelu/ros_workspaces/lab5/src/planning
-# Build directory: /home/akashvelu/ros_workspaces/lab5/build/planning
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
