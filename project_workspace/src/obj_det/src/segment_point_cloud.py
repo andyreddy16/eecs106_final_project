a4eae7921affced73b50a7ffd3997985de5a2da0
michael153 <m.wan@berkeley.edu>
2020-12-09 02:31:23 -0800

    Fix point cloud segmentation, add support for 3rd kinect

--- project_workspace/src/obj_det/src/segment_point_cloud.py
+++ project_workspace/src/obj_det/src/segment_point_cloud.py
@@ -21,145 +21,34 @@ from sensor_msgs import point_cloud2
 from sensor_msgs.msg import Image, CameraInfo, PointCloud2
 from geometry_msgs.msg import PointStamped
 
-from transforms import get_g_world_k1, get_g_world_k2
+from transforms import get_g_0k
 
-def filter(image, lower_thresh=(0, 50, 0), upper_thresh=(50, 255, 50)):
-    """Only keep the green ball and filter everything else out"""
-    image_copy = np.copy(image)
-    for i in range(3):
-        mask = ((image_copy[:,:,i] >= lower_thresh[i]).astype(int) * (image_copy[:,:,i] <= upper_thresh[i]).astype(int)).astype(bool)
-        image_copy[(1 - mask).astype(bool)] = 0
-    return image_copy.astype(np.uint8)
+# Account for camera frames being defined as +Z pointing outwards from cam
+KINECT_OFFSET = np.array([
+    [0, -1, 0, 0],
+    [0, 0, -1, 0.036],
+    [1, 0, 0, 0],
+    [0, 0, 0, 1]
+])
 
-def segment_pointcloud(points, segmented_image, cam_matrix, trans, rot):
-    print("Starting segment_pointcloud...")
-    IDX2D = lambda i, j, dj: dj * i + j
+FILTER_PRESETS = [
+    [(0, 60, 0), (40, 255, 40)],
+    [(0, 60, 0), (40, 255, 40)],
+    [(100, 150, 100), (255, 255, 255)]
+]
 
-    xyz = points.T
-
-    pixel_coords = project_points(xyz, cam_matrix, trans, rot)
-
-    image_h, image_w = segmented_image.shape[:2]
-    in_frame = ((0 <= pixel_coords[0]) & (pixel_coords[0] < image_w)
-                & (0 <= pixel_coords[1]) & (pixel_coords[1] < image_h))
-    points = points[in_frame]
-
-    if points is not None and len(points):
-        print("Found %d pc points that can be seen by camera" % len(points))
-
-    pixel_coords = pixel_coords[:, in_frame]
-
-    j, i = pixel_coords
-
-    greyscale_segmented_image = segmented_image[:,:,1]
-
-    linearized_pixel_coords = IDX2D(i, j, greyscale_segmented_image.shape[1])
-    linearized_segmentation = greyscale_segmented_image.reshape(-1)
-    point_labels = linearized_segmentation[linearized_pixel_coords]
-
-    print("Found %d pc points that match filtered image" % np.sum(point_labels != 0))
-
-    return points[point_labels != 0]
-
-def project_points(points, cam_matrix, trans, rot):
-    """
-    This funtion should perform the job of projecting the input pointcloud onto the frame
-    of an image captured by a camera with camera matrix as given, of dimensions as given,
-    in pixels.
-
-    points is an 3 x N array where the ith entry is an (x, y, z) point in 3D space, in 
-    the reference frame of the depth camera. This corresponds to the tf frame
-    camera_depth_optical_frame. However, the image is taken by an RGB camera, with
-    reference frame camera_color_optical_frame. (trans, rot) together give the translation
-    vector and rotation matrix that transform points in the depth camera frame to points
-    in the RGB camera frame.
-
-    For each point in points, compute the pixel co-ordinates (u, v) onto which that point
-    would be projected.
-
-    This function should return a 2 x N integer array of pixel co-ordinates. The ith entry 
-    should  be the index (u, v) of the pixel onto which the ith point in the pointcloud should 
-    get projected.
-
-    Use the point projection model introduced in the lab documentation to perform this
-    projection.
-
-    Note that this function should be able to operate on large pointclouds very efficiently.
-    Make good use of numpy functions to vectorize and to act on the entire pointcloud at once.
-
-    Args:
-    
-    points: (numpy.ndarray) Array of shape (3, N). ith entry is a 3D array representing
-            a single (x, y, z) point in the reference frame of the camera.
-
-    cam_matrix: (numpy.ndarray) Array of shape (3, 3) representing the camera intrinsic
-                matrix.
-
-                This parameter takes the standard form of the camera matrix as described
-                in the lab doc:
-
-                [[fx, s,  x0],
-                 [0,  fy, y0],
-                 [0,  0,  1 ]]
-
-    trans: (numpy.ndarray) 1D array of length 3. This is the translation vector that
-    offsets points in the depth camera reference frame to the RGB camera reference frame.
-
-    rot: (numpy.ndarray) array of shape (3, 3). This is the 3x3 rotation matrix that takes
-    points from the depth camera frame to the RGB camera frame.
-
-    """
-
-    # STEP 1: Transform pointcloud into new reference frame.
-    points = np.dot(rot, points) + trans[:, None]
-
-    # STEP 2: Project new pointcloud onto image frame using K matrix.
-    # gives a 3 x N array of image plane coordinates in homogenous coordinates.
-    homo_pixel_coords = np.dot(cam_matrix, points)
-
-    # STEP 3: Convert homogenous coordinates to regular 2D coordinates.
-    # To do this, you need to divide the first two coordinates of homo_pixel_coords
-    # by the third coordinate.
-    pixel_coords = homo_pixel_coords[:2, :] / homo_pixel_coords[2, :]
-
-    # STEP 4: Convert to integers. Take the floor of pixel_coords then cast it
-    # to an integer type, like numpy.int32
-    pixel_coords = np.floor(pixel_coords).astype(int)
-
-    return pixel_coords
-
-
-def isolate_object_of_interest(points, image, cam_matrix, trans, rot, cam_to_world_R, cam_to_world_T):
-    segmented_image = filter(image)
-    points = segment_pointcloud(points, segmented_image, cam_matrix, trans, rot)
-    
-    print("Isolated ball points in point cloud:")
-    print(points)
-
-    if points is not None and len(points):
-        centroid = np.median(points, axis=0)
-        print("Found centroid at (%.3f, %.3f, %.3f)" % (centroid[0], centroid[1], centroid[2]))
-        world_centroid = np.dot(cam_to_world_R, centroid) + cam_to_world_T.flatten()
-        print("In world coords: (%.3f, %.3f, %.3f)" % (world_centroid[0], world_centroid[1], world_centroid[2]))
-    else:
-        print("Ball not detected")
-    
-    print("\n")
-    print("\n")
-
-    return points
+def get_RT(G):
+    """Extracts R and T from G"""
+    R = G[:3, :3]
+    T = G[:3, 3].flatten()
+    return R, T
 
 def get_camera_matrix(camera_info_msg):
-    # TODO: Return the camera intrinsic matrix as a 3x3 numpy array
-    # by retreiving information from the CameraInfo ROS message.
-    # Hint: numpy.reshape may be useful here.
-    # print("HERE")
-    # print(camera_info_msg.K)
-    # print(np.reshape(np.array(camera_info_msg.K), (3,3)))
+    """Gets camera matrix from camera info msg"""
     return np.reshape(np.array(camera_info_msg.K), (3,3))
 
 def numpy_to_pc2_msg(points):
-
+    """Given Nx3 points, assemble into PointCloud2 data object"""
     data = np.zeros((points.shape[0],), dtype=[
         ('x', np.float32),
         ('y', np.float32),
@@ -174,105 +63,227 @@ def numpy_to_pc2_msg(points):
     data['r'] = 0
     data['g'] = 255
     data['b'] = 0
-
     return ros_numpy.msgify(PointCloud2, data, frame_id='world')
+
+def green_dominant_filter(image):
+    b,g,r = cv2.split(image)
+    target = g
+    other1 = b
+    other2 = r
+
+    # Figure out which ones we need to zero & zero them
+    should_zero = (target < 1.5*other1) | (target < 1.5*other2)
+    g[should_zero] = 0
+    r[should_zero] = 0
+    b[should_zero] = 0
+
+    # Merge channels back
+    return cv2.merge((b,g,r))
+
+def filter(image, lower_thresh=(0, 60, 0), upper_thresh=(40, 255, 40)):
+    """Only keep the green ball and filter everything else out"""
+    image = green_dominant_filter(image)
+    image_copy = np.copy(image)
+    # for i in range(3):
+    #     mask = ((image_copy[:,:,i] >= lower_thresh[i]).astype(int) * (image_copy[:,:,i] <= upper_thresh[i]).astype(int)).astype(bool)
+    #     image_copy[(1 - mask).astype(bool)] = 0
+    return image_copy.astype(np.uint8)
+
+def segment_pointcloud(points, segmented_image, cam_mat, trans, rot):
+    """Segment the pointcloud by finding relationship between
+    image_raw and pointcloud, and only keeping pc points that
+    correspond with nonzero pixels in filtered image.
+    """
+    print("Starting segment_pointcloud...")
+    IDX2D = lambda i, j, dj: dj * i + j # convert 2d coord to 1d
+    xyz = points.T # needs to be 3 x N
+
+    # project pc points onto image to find relationship
+    pixel_coords = project_points(xyz, cam_mat, trans, rot)
+
+    # only look at pc points that could be seen by image_raw
+    image_h, image_w = segmented_image.shape[:2]
+    in_frame = ((0 <= pixel_coords[0]) & (pixel_coords[0] < image_w)
+                & (0 <= pixel_coords[1]) & (pixel_coords[1] < image_h))
+    points = points[in_frame]
+
+    if points is not None and len(points):
+        print("Found %d pc points that can be seen by camera" % len(points))
+
+    pixel_coords = pixel_coords[:, in_frame]
+
+    j, i = pixel_coords
+
+    # get greyscale image (only take the 'G' channel)
+    greyscale_segmented_image = segmented_image[:,:,1]
+
+    # find all the points that correspond with nonzero value in greyscale_segmented_image
+    linearized_pixel_coords = IDX2D(i, j, greyscale_segmented_image.shape[1])
+    linearized_segmentation = greyscale_segmented_image.reshape(-1)
+    point_labels = linearized_segmentation[linearized_pixel_coords]
+
+    print("Found %d pc points that match filtered image" % np.sum(point_labels != 0))
+
+    return points[point_labels != 0]
+
+def project_points(points, cam_mat, trans, rot):
+    """Projects the input pointcloud onto the frame of an image captured
+    by a camera.
+    """
+    # transform pointcloud into new reference frame.
+    points = np.dot(rot, points) + trans[:, None]
+    # project new pointcloud onto image frame using K matrix.
+    homo_pixel_coords = np.dot(cam_mat, points)
+    # convert homogenous coordinates to regular 2D coordinates.
+    pixel_coords = homo_pixel_coords[:2, :] / homo_pixel_coords[2, :]
+    # convert to integers coords
+    pixel_coords = np.floor(pixel_coords).astype(int)
+    return pixel_coords
+
+def isolate_object_of_interest(points, img, cam_mat, depth_to_img_raw, vis=False, pub=None, filter_preset_id=0):
+    """Given an image and pointcloud, applies a filter to both image and pointcloud
+    """
+    depth_to_raw_R = depth_to_img_raw[:3, :3]
+    depth_to_raw_T = depth_to_img_raw[:3, 3].flatten()
+    segmented_img = filter(img, lower_thresh=FILTER_PRESETS[filter_preset_id][0], upper_thresh=FILTER_PRESETS[filter_preset_id][1])
+    if vis and pub:
+        pub.publish(ros_numpy.msgify(Image, segmented_img, 'rgb8'))
+    points = segment_pointcloud(points, segmented_img, cam_mat, depth_to_raw_T, depth_to_raw_R)    
+    if points is None or len(points) == 0:
+        return None
+    return points
+
 
 class PointcloudProcess:
     """
     Wraps the processing of a pointcloud from an input ros topic and publishing
     to another PointCloud2 topic.
-
     """
-    def __init__(self, points_sub_topic, 
-                       image_sub_topic,
-                       cam_info_topic,
-                       points_pub_topic):
+    def __init__(self, num_kinects=3):
+
+        self.NUM_KINECTS = num_kinects
+
+        PTS_TOPICS = ['/camera/kinect%d/depth/points' % (i+1) for i in range(self.NUM_KINECTS)]
+        CAM_INFO_TOPICS = ['/camera/kinect%d/color/camera_info' % (i+1) for i in range(self.NUM_KINECTS)]
+        RGB_IMG_TOPICS = ['/camera/kinect%d/color/image_raw' % (i+1) for i in range(self.NUM_KINECTS)]
+        FILTERED_IMG_TOPICS = ['/vision/pc/k%d_filtered_image' % (i+1) for i in range(self.NUM_KINECTS)]
+        SEGMENTED_PC_PUB_TOPIC = '/vision/pc/segmented_points'
+
+        assert len(PTS_TOPICS) == self.NUM_KINECTS
+        assert len(CAM_INFO_TOPICS) == self.NUM_KINECTS
+        assert len(RGB_IMG_TOPICS) == self.NUM_KINECTS
+
+        # Transform b/w world and kinect for each kinect
+        self.G0k = [get_g_0k(i) for i in range(self.NUM_KINECTS)]
+
+        for i in range(self.NUM_KINECTS):
+            self.G0k[i] = np.dot(self.G0k[i], np.linalg.inv(KINECT_OFFSET))
+
+        # Extract R and T for each transform
+        self.G0k_R = []
+        self.G0k_T = []
+
+        for i in range(self.NUM_KINECTS):
+            R, T = get_RT(self.G0k[i])
+            self.G0k_R.append(R)
+            self.G0k_T.append(T)
 
         self.num_steps = 0
-
         self.messages = deque([], 5)
-        self.pointcloud_frame = None
 
-        points_sub = message_filters.Subscriber(points_sub_topic, PointCloud2)
-        image_sub = message_filters.Subscriber(image_sub_topic, Image)
-        caminfo_sub = message_filters.Subscriber(cam_info_topic, CameraInfo)
+        # Pointcloud, Raw image, and Cam Info subscribers
+        self.pts_subs = [message_filters.Subscriber(PTS_TOPICS[i], PointCloud2) for i in range(self.NUM_KINECTS)]
+        self.img_subs = [message_filters.Subscriber(RGB_IMG_TOPICS[i], Image) for i in range(self.NUM_KINECTS)]
+        self.cam_info_subs = [message_filters.Subscriber(CAM_INFO_TOPICS[i], CameraInfo) for i in range(self.NUM_KINECTS)]
 
         self._bridge = CvBridge()
         self.listener = tf.TransformListener()
         
-        self.points_pub = rospy.Publisher(points_pub_topic, PointCloud2, queue_size=10)
-        # self.image_pub = rospy.Publisher('segmented_image', Image, queue_size=10)
-
-        self.filtered_pub = rospy.Publisher('/vision/pc/filtered_image', Image, queue_size=10)
+        self.segmented_pc_pub = rospy.Publisher(SEGMENTED_PC_PUB_TOPIC, PointCloud2, queue_size=10)
+        self.filtered_pubs = [rospy.Publisher(FILTERED_IMG_TOPICS[i], Image, queue_size=10) for i in range(self.NUM_KINECTS)]
         
-        ts = message_filters.ApproximateTimeSynchronizer([points_sub, image_sub, caminfo_sub],
-                                                          10, 0.1, allow_headerless=True)
-        
-        self.G01 = get_g_world_k1()
+        message = []
+        for i in range(self.NUM_KINECTS):
+            message.extend([self.pts_subs[i], self.img_subs[i], self.cam_info_subs[i]])
 
-        # self.G01 = np.dot(self.G01, self.G_prime)
-
-        # self.camera_offset = np.array([
-        #     [0, -1, 0, 0],
-        #     [0, 0, -1, 0],
-        #     [1, 0, 0, 0],
-        #     [0, 0, 0, 1]
-        # ])
-
-        self.camera_offset = np.array([
-            [0, 0, 1, 0],
-            [0, 1, 0, 0],
-            [-1, 0, 0, 0],
-            [0, 0, 0, 1]
-        ])
-
-        self.G01 = np.dot(self.G01, self.camera_offset)
-
-        self.G01_R = self.G01[:3, :3]
-        self.G01_T = self.G01[:3, 3].flatten()
-
+        ts = message_filters.ApproximateTimeSynchronizer(message, 10, 0.1, allow_headerless=True)
         ts.registerCallback(self.callback)
 
-    def callback(self, points_msg, image, info):
+    def callback(self, k1_pts=None, k1_img=None, k1_info=None,
+            k2_pts=None, k2_img=None, k2_info=None,
+            k3_pts=None, k3_img=None, k3_info=None
+        ):
+        """Callback method for ApproximateTimeSynchronizer with optional parameters to
+        support 2 Kinects and 3 Kinects"""
+        params = locals()
+        pts_msgs = []
+        img_msgs = []
+        info_msgs= []
+        intrinsics = []
+        rgb_imgs = []
+        pc_points = []
+        message = []
+
+        for i in range(self.NUM_KINECTS):
+            pts_msgs.append(params["k%d_pts" % (i+1)])
+            img_msgs.append(params["k%d_img" % (i+1)])
+            info_msgs.append(params["k%d_info" % (i+1)])
         try:
-            intrinsic_matrix = get_camera_matrix(info)
-            rgb_image = ros_numpy.numpify(image)
-            cloud_points = np.array(list(point_cloud2.read_points(points_msg, skip_nans=True, field_names = ("x", "y", "z"))))
+            for i in range(self.NUM_KINECTS):
+                intrinsics.append(get_camera_matrix(info_msgs[i]))
+                rgb_imgs.append(ros_numpy.numpify(img_msgs[i]))
+                pc_points.append(np.array(list(point_cloud2.read_points(pts_msgs[i], skip_nans=True, field_names=("x", "y", "z")))))
+            for i in range(self.NUM_KINECTS):
+                message.extend([pc_points[i], rgb_imgs[i], intrinsics[i]])
         except Exception as e:
             rospy.logerr(e)
             return
-
-        self.messages.appendleft((cloud_points, rgb_image, intrinsic_matrix))
-        self.publish_once_from_queue()
+        self.messages.appendleft(tuple(message))
+        self.process_point_cloud()
         self.num_steps += 1
-        
+        return
 
-    def publish_once_from_queue(self):
-        if self.messages:
-            points, image, info = self.messages.pop()
+    def process_point_cloud(self):
+        """Process point cloud data given image and camera intrinsic matrix info"""
+        if not self.messages:
+            return
+        message = self.messages.pop()
+        pts = []
+        imgs = []
+        infos = []
 
-            if points is not None and len(points):
-                points = isolate_object_of_interest(points, image, info, 
-                    np.zeros(3), np.eye(3),
-                    self.G01_R, self.G01_T)
+        for i in range(self.NUM_KINECTS):
+            pts.append(message[3*i])
+            imgs.append(message[3*i + 1])
+            infos.append(message[3*i + 2])
 
-                if points is not None and points.shape[0]:
-                    # print(points)
-                    points_msg = numpy_to_pc2_msg(points)
+        cumulative_world_pts = []
+        cumulative_world_centroid = None
 
-                    self.points_pub.publish(points_msg)
-                    # print("Published segmented pointcloud at timestamp:",
-                           # points_msg.header.stamp.secs)
+        for i in range(self.NUM_KINECTS):
+            if pts[i] is not None and len(pts[i]):
+                pts[i] = isolate_object_of_interest(pts[i], imgs[i], infos[i],
+                    np.eye(4), vis=True, pub=self.filtered_pubs[i], filter_preset_id=i)
+                if pts[i] is not None:
+                    world_pts = np.dot(self.G0k_R[i], pts[i].T).T + self.G0k_T[i]
+                    world_centroid = np.median(world_pts, axis=0)
+                    print("Centroid found by Kinect %d: (%.3f, %.3f, %.3f)" % (i, world_centroid[0], world_centroid[1], world_centroid[2]))
+                    cumulative_world_pts.extend(world_pts)
+
+        if cumulative_world_pts:
+            cumulative_world_pts = np.array(cumulative_world_pts)
+            cumulative_world_centroid = np.median(cumulative_world_pts, axis=0)
+            print("Cumulative centroid: (%.3f, %.3f, %.3f)" % (cumulative_world_centroid[0],
+                cumulative_world_centroid[1], cumulative_world_centroid[2]))
+            world_points_msg = numpy_to_pc2_msg(cumulative_world_pts)
+            self.segmented_pc_pub.publish(world_points_msg)
+
+        print("\n")
+
 
 def main():
-    CAM_INFO_TOPIC = '/camera/kinect1/color/camera_info'
-    RGB_IMAGE_TOPIC = '/camera/kinect1/color/image_raw'
-    POINTS_TOPIC = '/camera/kinect1/depth/points'
-    POINTS_PUB_TOPIC = 'segmented_points'
-
     rospy.init_node('pointcloud_segmenter')
-    process = PointcloudProcess(POINTS_TOPIC, RGB_IMAGE_TOPIC,
-                                CAM_INFO_TOPIC, POINTS_PUB_TOPIC)
+    process = PointcloudProcess(num_kinects=3)
     rospy.spin()
 
 if __name__ == '__main__':
--- project_workspace/src/obj_det/src/transforms.py
+++ project_workspace/src/obj_det/src/transforms.py
@@ -4,33 +4,33 @@ Calculate transforms between frames
 """
 
 import numpy as np
-
-# KINECT_1_POSE = [2.6, 0.5, 0.9, 0, -0.35, -2.773]
-KINECT_2_POSE = [2.6, 1.0, 0.9, 0, -0.35, -2.484479]
-
-KINECT_1_POSE = [0.1, 0, 0.95, 0, -0.3, 0]
-# KINECT_2_POSE = [0.83, -1.3, 0.95, 0, -0.3, 1.3]
-
 from tf.transformations import quaternion_from_euler
 from scipy.spatial.transform import Rotation as R
 
-def get_transform(T, r, p, y):
+# for point_cloud_bounce_two_kinects.bag
+KINECT_POSES = [
+	[0.1, 0, 0.95, 0, -0.3, 0],
+	[2.6, 0.5, 0.9, 0, -0.35, -2.773],
+	[1.0, -1.0, 3.0, 0, 1.0, 1.2]
+]
+
+def get_transform(angles, T):
+	r = angles[0]
+	p = angles[1]
+	y = angles[2]
 	q = quaternion_from_euler(r, p, y)
-	r = R.from_quat(q)
+	rot = R.from_quat(q)
 	g = np.zeros((4, 4))
-	g[:3, :3] = r.as_dcm()
+	g[:3, :3] = rot.as_dcm()
 	g[:3, 3] = T.flatten()
 	g[3, 3] = 1
 	return g
 
-def get_g_world_k1():
-	T = np.array(KINECT_1_POSE[:3])
-	return get_transform(T, KINECT_1_POSE[3], KINECT_1_POSE[4], KINECT_1_POSE[5])
-
-def get_g_world_k2():
-	T = np.array(KINECT_2_POSE[:3])
-	return get_transform(T, KINECT_2_POSE[3], KINECT_2_POSE[4], KINECT_2_POSE[5])
+def get_g_0k(k):
+	angles = np.array(KINECT_POSES[k][3:])
+	T = np.array(KINECT_POSES[k][:3])
+	return get_transform(angles, T)
 
 if __name__ == "__main__":
-	print(get_g_world_k1())
-	print(get_g_world_k2())
+	print(get_g_0k(0))
+	print(get_g_0k(1))
